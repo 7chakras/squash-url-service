89e8cbd890c1ebba797ea7291a3580cb3cbfa941
pareenvatani <pareenvatani@hotmail.com>
2019-06-18 23:44:33 +1000

    basic test for CI CD

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 1337;
 app.get("/", (req, res) => res.send(process.env));
+app.get("/test", (req, res) => res.send('test'+process.env.NODE_ENV));
 
 app.listen(port, (req, res) => {
   console.log(`Example app listening on port ${port} ${process.env.NODE_ENV}!`);
-
 });
