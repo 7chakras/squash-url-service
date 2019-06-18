803f6442b88cce108669ca5715c6c5b417ecea07
pareenvatani <pareenvatani@hotmail.com>
2019-06-18 20:57:24 +1000

    testing out environment vars

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 1337;
+app.get("/", (req, res) => res.send(process.env));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, (req, res) => {
+  console.log(`Example app listening on port ${port} ${process.env.NODE_ENV}!`);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+});
