d0a315e7dec5db6fb178a488e49b2fb7be10d76e
pareenvatani <pareenvatani@hotmail.com>
2019-06-19 16:20:33 +1000

    modified line break in eslint config

--- src/core/configs/app-config.js
+++ src/core/configs/app-config.js
@@ -1,18 +1,17 @@
 const appConfigDev = {
   service: {
-    appBasePath: "http://localhost:4000",
+    appBasePath: 'http://localhost:4000',
     port: process.env.PORT || 3000,
-    environment: process.env.NODE_ENV
-  }
+    environment: process.env.NODE_ENV,
+  },
 };
 
 const appConfigProd = {
   service: {
     appBasePath: `https://${process.env.WEBSITE_HOSTNAME}`,
     port: process.env.PORT,
-    environment: process.env.NODE_ENV
-  }
+    environment: process.env.NODE_ENV,
+  },
 };
 
-module.exports =
-  process.env.NODE_ENV === "production" ? appConfigProd : appConfigDev;
+module.exports = process.env.NODE_ENV === 'production' ? appConfigProd : appConfigDev;
